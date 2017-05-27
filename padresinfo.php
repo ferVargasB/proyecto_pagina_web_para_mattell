45b07cbb2b54b74865771c29bf691fb061e0d827
fernando <fernandovargas@MacBook-Pro-de-Fernando.local>
2017-05-27 02:40:25 -0500

    Prueba de validación de los campos
    Error al guardar la variable “paterno”

--- padresinfo.php
+++ padresinfo.php
@@ -11,33 +11,17 @@ $correo = $_POST["correo"];
 $direccion = $_POST["direccion"];
 $usuarioo = $_POST["usuario"];
 $pass = $_POST["pass"];
-
-echo "xxxxx";
 //variables del servidor
 $servidor="localhost";
 $basededatos="kindermattell";
 $usuario="root";
 $contrasenia="root";
 
-
-function validarCampos($nombreP, $paternoP, $maternoP, $correoP, $direccionP, $usuarioP, $passP){
-
-	if(preg_match("/^[a-z]+$/i", $nombreP) and preg_match("/^[a-z]+$/i", $paternoP) and  preg_match("/^[a-z]+$/i", maternoP) and
-	 preg_match("/([a-z]+) ([0-9]+)$/", $direccionP) and filter_var($correoP, FILTER_VALIDATE_EMAIL) and preg_match("/^\w+$/", $usuarioP) and
-	 preg_match('/(?=[A-Z])/', $passP))
-
-	return true;
-
-	else {
-		false;
-	}
-}
-
 	try {
 
 			$conexionMysqli = new mysqli($servidor,$usuario, $contrasenia , $basededatos);
 			echo "2";
-			if ($conexionMysqli-> connect_errno) {
+			if ($conexionMysqli-> connect_errno ) {
 				echo "Fallo la conexión con MYSQL:
 				(" . $conexionMysqli -> connect_errno . ")
 				" . $conexionMysqli -> connect_error;
@@ -47,12 +31,16 @@ function validarCampos($nombreP, $paternoP, $maternoP, $correoP, $direccionP, $u
 				$query = "insert into usuarios
 				(nombre, contrasena, rol_id) values ('".$usuarioo."', '".$contrasena."', 2)";
 				$resultadooperacion = $conexionMysqli->query($query);
-				echo "Funcionó los primis :v";
 
-				if ( validarCampos($nombre, $paterno, $materno, $correo, $direccion, $usuarioo, $pass) ) {
-					#si los datos estan correctos, entonces, se hace el query
-					if ($resultadooperacion) {
-						#si el query es correcto, se insertan los datos
+
+				#comprobar si el usuario existe
+				if ($resultadooperacion) {
+
+					echo $paterno;
+
+					#comprobar si los datos son correctos
+					if ( preg_match("/^[a-z]+$/i", $nombre) && preg_match("/^[a-z]+$/i", $materno) && filter_var($correo, FILTER_VALIDATE_EMAIL) && preg_match("/^\w+$/", $usuarioo) && preg_match('/(?=[A-Z])/', $pass) ){
+
 						$query = "insert into padres
 						(nombretutor,apellidopaterno, apellidomaterno, correo, direccion, usuario_id) values (
 						'".$nombre."','".$paterno."',
@@ -76,21 +64,12 @@ function validarCampos($nombreP, $paternoP, $maternoP, $correoP, $direccionP, $u
 								</div>
 							<?php
 						}
-					} else{
-						?>
-							<div class = "text-success text-center">
-								<p>Operaci&oacute;n <b>NO</b> Realizada </p>
-							</div>
-						<?php
+
+					} else {
+						echo "no funcionó la operación";
 					}
 
-				} else {
-					# campos erroneos
-					<div class = "text-success text-center">
-						<p>Operaci&oacute;n <b>NO</b> Realizada </p>
-					</div>
 				}
-
 			}
 		}
 		catch (Exception $e)
