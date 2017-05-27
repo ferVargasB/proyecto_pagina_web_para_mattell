be79beed444c5f6c71845b51e80e2aa9e9fec224
fernando <fernandovargas@MacBook-Pro-de-Fernando.local>
2017-05-27 00:19:08 -0500

    Se añadió función para validar datos
    Primera prueba de validación de datos de este formulario

--- padresinfo.php
+++ padresinfo.php
@@ -19,8 +19,22 @@ $basededatos="kindermattell";
 $usuario="root";
 $contrasenia="root";
 
+
+function validarCampos($nombreP, $paternoP, $maternoP, $correoP, $direccionP, $usuarioP, $passP){
+
+	if(preg_match("/^[a-z]+$/i", $nombreP) and preg_match("/^[a-z]+$/i", $paternoP) and  preg_match("/^[a-z]+$/i", maternoP) and
+	 preg_match("/([a-z]+) ([0-9]+)$/", $direccionP) and filter_var($correoP, FILTER_VALIDATE_EMAIL) and preg_match("/^\w+$/", $usuarioP) and
+	 preg_match('/(?=[A-Z])/', $passP))
+
+	return true;
+
+	else {
+		false;
+	}
+}
+
 	try {
-		
+
 			$conexionMysqli = new mysqli($servidor,$usuario, $contrasenia , $basededatos);
 			echo "2";
 			if ($conexionMysqli-> connect_errno) {
@@ -29,31 +43,39 @@ $contrasenia="root";
 				" . $conexionMysqli -> connect_error;
 			}
 			else{
-				
-				$query = "insert into usuarios 
+
+				$query = "insert into usuarios
 				(nombre, contrasena, rol_id) values ('".$usuarioo."', '".$contrasena."', 2)";
 				$resultadooperacion = $conexionMysqli->query($query);
 				echo "Funcionó los primis :v";
-				
-				
-				if ($resultadooperacion) {
-					
-					$query = "insert into padres 
-					(nombretutor,apellidopaterno, apellidomaterno, correo, direccion, usuario_id) values (
-					'".$nombre."','".$paterno."', 
-					'".$materno."', '".$correo."', '".$direccion."', (select usuario_id from usuarios where nombre='".$usuarioo."' and contrasena='".$contrasena."'))";
-					
-					$resultadooperacion = $conexionMysqli->query($query);
-						
+
+				if ( validarCampos) {
+					#si los datos estan correctos, entonces, se hace el query
 					if ($resultadooperacion) {
-					?>
-						<div class = "text-success text-center">
-							<p>Operaci&oacute;n Realizada con &eacute;xito</p>
-						</div>
-						<script type="text/javascript">
-						window.location="datosalumnos.php"; 
-						</script>
-						<?php
+						#si el query es correcto, se insertan los datos
+						$query = "insert into padres
+						(nombretutor,apellidopaterno, apellidomaterno, correo, direccion, usuario_id) values (
+						'".$nombre."','".$paterno."',
+						'".$materno."', '".$correo."', '".$direccion."', (select usuario_id from usuarios where nombre='".$usuarioo."' and contrasena='".$contrasena."'))";
+
+						$resultadooperacion = $conexionMysqli->query($query);
+
+						if ($resultadooperacion) {
+						?>
+							<div class = "text-success text-center">
+								<p>Operaci&oacute;n Realizada con &eacute;xito</p>
+							</div>
+							<script type="text/javascript">
+							window.location="datosalumnos.php";
+							</script>
+							<?php
+						} else{
+							?>
+								<div class = "text-success text-center">
+									<p>Operaci&oacute;n <b>NO</b> Realizada </p>
+								</div>
+							<?php
+						}
 					} else{
 						?>
 							<div class = "text-success text-center">
@@ -61,18 +83,17 @@ $contrasenia="root";
 							</div>
 						<?php
 					}
-				} else{
-					?>
-						<div class = "text-success text-center">
-							<p>Operaci&oacute;n <b>NO</b> Realizada </p>
-						</div>
-					<?php
+					
+				} else {
+					# campos erroneos
+					<div class = "text-success text-center">
+						<p>Operaci&oacute;n <b>NO</b> Realizada </p>
+					</div>
 				}
-				
+
 			}
-		}	 
-		catch (Exception $e) 
+		}
+		catch (Exception $e)
 		{
 			echo $e;
 		}
-		
